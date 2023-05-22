c413fec8c2e7ed1834e300fd18df648cb11c2a48
AyumuSaito <awe.nun02@gmail.com>
2023-05-22 17:35:04 +0900

    lang_syntax_simple

--- theories/lang_syntax_simplified.v
+++ theories/lang_syntax_simplified.v
@@ -5,6 +5,15 @@ From mathcomp.classical Require Import functions cardinality fsbigop.
 Require Import signed reals ereal topology normedtype sequences esum measure.
 Require Import lebesgue_measure numfun lebesgue_integral kernel.
 
+(**************************************************************)
+(*        Various types of the definition of syntax           *)
+(* lang_extrinsic : non-intrinsic definition of expression    *)
+(* lang_intrinsic_ty : intrinsically-typed syntax             *)
+(* lang_intrinsic_sc : intrinsically-scoped syntax            *)
+(* lang_intrinsic_tysc : intrinsically-typed/scoped syntax    *)
+(*                                                            *)
+(**************************************************************)
+
 Set Implicit Arguments.
 Implicit Types V : Set.
 Unset Strict Implicit.
@@ -101,13 +110,13 @@ Fail Example e3 := [Let x <~ {1%:R}:r In
                Let y <~ {2%:R}:r In 
                %{x} + %{y}].
 
-Fixpoint varof (l : seq Ty) (i : nat) :
+Fixpoint acc (l : seq Ty) (i : nat) :
   typei R (TList l) -> @typei R (nth TUnit l i) :=
   match l return (typei R (TList l) -> typei R (nth TUnit l i)) with
   | [::] => match i with | O => id | j.+1 => id end
   | _ :: _ => match i with
                | O => fst
-               | j.+1 => fun H => varof j H.2
+               | j.+1 => fun H => acc j H.2
                end
   end.
 
@@ -118,7 +127,7 @@ Inductive eval : forall (g : Ctx) (t : Ty), exp -> (typei R (TList (map snd g)) 
     @eval g TReal e2 (fun=> v2) -> 
     @eval g TReal (Plus e1 e2) (fun=> (v1 + v2)%R)
 (* | EVVar (g : Ctx) (t : Ty) (x : string) : 
-  let i := seq.index x (map fst g) in @eval g t (Var x) (@varof (map snd g) i) *)
+  let i := seq.index x (map fst g) in @eval g t (Var x) (@acc (map snd g) i) *)
 (* | EVLetin (g : Ctx) (t : Ty) (x : string) e1 e2 v1 (v2 : R -> R) :
     @eval g TReal e1 (fun=> v1) ->
     @eval ((x, TReal) :: g) TReal e2 (fun x => v2 x) ->
@@ -130,8 +139,8 @@ Fail Compute eval [::] (Const 1) (fun=> 1%R).
 End lang_extrinsic.
 End lang_extrinsic.
 
-Module lang_intrinsic.
-Section lang_intrinsic.
+Module lang_intrinsic_ty.
+Section lang_intrinsic_ty.
 Variables (R : realType).
 
 Section exp.
@@ -171,28 +180,28 @@ Fail Example e3 := [Let x <~ {1%:R}:r In
                Let y <~ {2%:R}:r In 
                %{x} + %{y}] : exp _.
 
-Fixpoint varof (l : seq Ty) (i : nat) :
+Fixpoint acc (l : seq Ty) (i : nat) :
   typei R (TList l) -> @typei R (nth TUnit l i) :=
   match l return (typei R (TList l) -> typei R (nth TUnit l i)) with
   | [::] => match i with | O => id | j.+1 => id end
   | _ :: _ => match i with
                | O => fst
-               | j.+1 => fun H => varof j H.2
+               | j.+1 => fun H => acc j H.2
                end
   end.
 
 Fail Inductive eval : forall (g : Ctx) (t : Ty), exp t -> (typei R (TList (map snd g)) -> typei R t) -> Prop :=
 | EVReal g c : @eval g _ (Real c) (fun=> c)
-| EVPlus g (e1 e2 : exp TReal) (v1 v2 : R) : 
-    @eval g TReal e1 (fun=> v1) -> 
-    @eval g TReal e2 (fun=> v2) -> 
-    @eval g TReal (Plus e1 e2) (fun=> (v1 + v2)%R)
-| EVVar (g : Ctx) (x : string) i : 
-    i = seq.index x (map fst g) -> eval (Var x) (@varof (map snd g) i)
-| EVLetin (g : Ctx) (t : Ty) (x : string) e1 e2 v1 (v2 : _ -> R) :
-    @eval g TReal e1 (fun=> v1) ->
-    @eval ((x, TReal) :: g) TReal e2 (fun x => v2 x) ->
-    @eval g TReal (Letin x e1 e2) (fun=> v2 v1)
+| EVPlus g (e1 e2 : exp TReal) (v1 v2 : _ -> _) : 
+    @eval g TReal e1 v1 -> 
+    @eval g TReal e2 v2 -> 
+    @eval g TReal (Plus e1 e2) (fun x => v1 x + v2 x)%R
+(* | EVVar (g : Ctx) (x : string) i : 
+    i = seq.index x (map fst g) -> eval (Var x erefl) (@acc (map snd g) i) *)
+| EVLetin (g : Ctx) (t : Ty) (x : string) e1 e2 (v1 v2 : _ -> _) :
+    @eval g TReal e1 v1 ->
+    @eval ((x, TReal) :: g) TReal e2 v2 ->
+    @eval g TReal (Letin x e1 e2) (v2 \o v1)
 .
 
 (* Goal @eval [::] _ [{1%R}:r] (fun=> 1%R).
@@ -204,11 +213,11 @@ Proof. exact/EVVar. Qed.
 Goal @eval [::] _ [Let x <~ {1%R}:r In %{"x"} + {2%R}:r] (fun=> 3).
 Proof. Abort. *)
 
-End lang_intrinsic.
-End lang_intrinsic.
+End lang_intrinsic_ty.
+End lang_intrinsic_ty.
 
-Module lang_intrinsic_ctx.
-Section lang_intrinsic_ctx.
+Module lang_intrinsic_sc.
+Section lang_intrinsic_sc.
 Variables (R : realType).
 
 Section exp.
@@ -279,8 +288,8 @@ Goal @eval [:: ("x", TReal)] _ [% {"x"}] (@varof [:: TReal] 0).
 Proof. exact/EVVar. Qed.
 Check [Let x <~ {1%R}:r In %{"x"} + {2%R}:r].
 
-End lang_intrinsic_ctx.
-End lang_intrinsic_ctx.
+End lang_intrinsic_sc.
+End lang_intrinsic_sc.
 
 Module lang_intrinsic_full.
 Section lang_intrinsic_full.
@@ -329,38 +338,51 @@ Example e2'' := [Let x <~ {1%:R}:r In Let y <~ %{"x"} In %{"y"}] : exp [::] _.
                Let y <~ {2%:R}:r In
                %{x} + %{y}] : exp [::] TReal. *)
 
-Fixpoint varof (l : seq Ty) (i : nat) :
+Fixpoint acc (l : seq Ty) (i : nat) :
   typei R (TList l) -> @typei R (nth TUnit l i) :=
   match l return (typei R (TList l) -> typei R (nth TUnit l i)) with
   | [::] => match i with | O => id | j.+1 => id end
   | _ :: _ => match i with
                | O => fst
-               | j.+1 => fun H => varof j H.2
+               | j.+1 => fun H => acc j H.2
                end
   end.
 
-Inductive eval : forall (g : Ctx) (t : Ty), exp g t -> (typei R (TList (map snd g)) -> typei R t) -> Prop :=
-| EVReal g c : @eval g _ [c:r] (fun=> c)
-| EVPlus g (e1 e2 : exp g TReal) (v1 v2 : R) : 
-    @eval g _ e1 (fun=> v1) -> 
-    @eval g _ e2 (fun=> v2) -> 
-    @eval g _ [e1 + e2] (fun=> (v1 + v2)%R)
-| EVVar (g : Ctx) (x : string) :
-    (* let i := seq.index x (map fst g) in eval (Var x) (@varof (map snd g) i) *)
-    let i := seq.index x (map fst g) in eval (Var x erefl) (@varof (map snd g) i)
-(* | EVLetin (g : Ctx) (t : Ty) (x : string) e1 e2 v1 v2 :
-    @eval g TReal e1 (fun=> v1) ->
-    @eval ((x, TReal) :: g) _ e2 (fun=> v2) ->
-    @eval g TReal (Letin x e1 e2) (fun=> v2 v1) *)
+Definition coucou (g : Ctx) (t t' : Ty) (e1 : exp g t) x (e2 :exp ((x, t)::g) t') (v1 : typei R (TList (map snd g)) -> typei R t)
+(v2 : typei R (TList (map snd ((x,t)::g))) -> typei R t')
+: typei R (TList (map snd (g))) -> typei R t'.
+move=> a.
+apply v2.
+split.
+apply: v1.
+exact: a.
+Show Proof.
+done.
+Show Proof.
+Abort.
+
+Program Inductive eval : forall (g : Ctx) (t : Ty), exp g t -> (typei R (TList (map snd g)) -> typei R t) -> Prop :=
+| EVReal g c : @eval g _ (Real c) (fun=> c)
+| EVPlus g (e1 e2 : exp g TReal) v1 v2 : 
+    @eval g TReal e1 v1 -> 
+    @eval g TReal e2 v2 -> 
+    @eval g TReal (Plus e1 e2) (fun x => v1 x + v2 x)%R
+| EVVar (g : Ctx) (x : string) : 
+    let i := seq.index x (map fst g) in eval (Var x erefl) (@acc (map snd g) i)
+| EVLetin (g : Ctx) (t t' : Ty) (x : string) (e1 : exp g t) (e2 : exp _ t') v1 v2 :
+    @eval g t e1 v1 ->
+    @eval ((x, t) :: g) t' e2 v2 ->
+    @eval g t' (Letin x e1 e2) (fun a => v2 (v1 a, a))
 .
 
 Goal @eval [::] _ [{1%R}:r] (fun=> 1%R).
 Proof. exact/EVReal. Qed.
 Goal @eval [::] _ [{1%R}:r + {2%R}:r] (fun=> 3%R).
 Proof. exact/EVPlus/EVReal/EVReal. Qed.
-Goal @eval [:: ("x", TReal)] _ [% {"x"}] (@varof [:: TReal] 0).
+Goal @eval [:: ("x", TReal)] _ [% {"x"}] (@acc [:: TReal] 0).
 Proof. exact/EVVar. Qed.
-(* Goal @eval [::] _ [Let x <~ {1%R}:r In %{"x"} + {2%R}:r]. *)
+Goal @eval [::] _ [Let x <~ {1%R}:r In %{"x"}] (fun=> 1%R).
+Proof. exact: (EVLetin (EVReal _ _) (EVVar _ _)). Qed.
 
 End lang_intrinsic_full.
 End lang_intrinsic_full.
@@ -382,6 +404,7 @@ Inductive exp : Ctx -> Ty -> Type :=
 End exp.
 
 Arguments Real {g}.
+Arguments Plus {g}.
 Arguments Var {g t}.
 Arguments Letin {g t u}.
 
@@ -389,7 +412,7 @@ Declare Custom Entry exp.
 Notation "[ e ]" := e (e custom exp at level 5) : easylang_scope.
 Notation "x ':r'" := (Real x%R) (in custom exp at level 1) 
   : easylang_scope.
-Notation "x + y" := (Plus x y) (in custom exp at level 1) 
+Notation "x + y" := (Plus x y) (in custom exp at level 2) 
   : easylang_scope.
 Notation "% x" := (Var x erefl) (in custom exp at level 1) 
   : easylang_scope.
@@ -403,11 +426,13 @@ Notation "{ x }" := x (in custom exp, x constr) : easylang_scope.
 Notation "x" := x (in custom exp at level 0, x ident) : easylang_scope.
 
 Local Open Scope easylang_scope.
+
 Section example.
 
-Example e0 := [{1%:R}:r] : exp [::] _.
+Example e0 := [{1%R}:r + {2%R}:r] : exp [::] _.
 Example e1 := [Let x <~ {1%:R}:r In %{"x"}] : exp [::] _.
 Example e2 := [Let x <~ {1%:R}:r In Let y <~ %{"x"} In %{"y"}] : exp [::] _.
+
 Fail Example e3 := [Let x <~ {1%:R}:r In
                Let y <~ {2%:R}:r In
                %{x} + %{y}] : exp [::] TReal.
@@ -450,87 +475,64 @@ Definition Var' (x : string) (t : Ty) (g : find x t) :=
 
 Notation "%1 x" := (@Var' x%string _ _) (in custom exp at level 1) : easylang_scope.
 
-(* Example e1 := [Let x <~ {1%:R}:r In %1{"x"}] : exp [::] _. *)
+Fixpoint acc (l : seq Ty) (i : nat) :
+  typei R (TList l) -> @typei R (nth TUnit l i) :=
+  match l return (typei R (TList l) -> typei R (nth TUnit l i)) with
+  | [::] => match i with | O => id | j.+1 => id end
+  | _ :: _ => match i with
+               | O => fst
+               | j.+1 => fun H => acc j H.2
+               end
+  end.
 
-(* Example e2 := [Let "x" <~ {1%:R}:r In Let "y" <~ %{"x"} In %{"y"}] : exp [::] TReal. *)
+Program Inductive eval : forall (g : Ctx) (t : Ty), exp g t -> (typei R (TList (map snd g)) -> typei R t) -> Prop :=
+| EVReal g c : @eval g _ (Real c) (fun=> c)
+| EVPlus g (e1 e2 : exp g TReal) v1 v2 : 
+    @eval g TReal e1 v1 -> 
+    @eval g TReal e2 v2 -> 
+    @eval g TReal (Plus e1 e2) (fun x => v1 x + v2 x)%R
+| EVVar (g : Ctx) (x : string) (i := seq.index x (map fst g)) : 
+    eval (Var x erefl) (@acc (map snd g) i)
+(* | EVVar' (g : Ctx) (x : string) : 
+    let i := seq.index x (map fst g) in eval [%1 x] (@acc (map snd g) i) *)
+| EVLetin (g : Ctx) (t t' : Ty) (x : string) (e1 : exp g t) (e2 : exp _ t') v1 v2 :
+    @eval g t e1 v1 ->
+    @eval ((x, t) :: g) t' e2 v2 ->
+    @eval g t' (Letin x e1 e2) (fun a => v2 (v1 a, a))
+.
 
-Goal (0 = seq.index 1 [:: 1; 2])%nat.
-exact: erefl.
-Abort.
-Goal False.
-evar (a : list nat).
-have : (0 = seq.index 1 (1 :: a))%nat.
-exact: erefl.
-(* Print a. *)
-Abort.
+Example e2' := [Let x <~ {1%:R}:r In
+               Let y <~ {2%:R}:r In 
+               %{x}] : exp [::] _.
+Goal eval e2' (fun=> 1%R).
+Proof.
+apply: (EVLetin (EVReal _ 1%R) (EVLetin (EVReal _ 2%R) (EVVar _ "x"))).
+Qed.
 
 Example e3' := Letin "x" (Real 1%:R) 
               (Letin "y" (Real 2%:R) 
               (Plus (@Var [:: ("y", TReal); ("x", TReal)] TReal x erefl) (Var "y" erefl))) : exp [::] _.
-               
-Example e4 := [Let x <~ {1%:R}:r In
+
+Lemma eval3' : @eval [::] TReal e3' (fun=> 3%R).
+Proof.
+exact: (EVLetin (EVReal [::] 1%R) (EVLetin (EVReal [:: ("x", TReal)] 2%R) (EVPlus (EVVar [:: ("y", TReal); ("x", TReal)] "x") (EVVar [:: ("y", TReal); ("x", TReal)] "y")))).
+Qed.
+
+Example e3 := [Let x <~ {1%:R}:r In
                Let y <~ {2%:R}:r In 
                %1{x} + %1{y}] : exp [::] _.
+
+Lemma eval3 : @eval [::] TReal e3 (fun=> 3%R).
+Proof.
+have:= (EVLetin (EVReal [::] 1%R) (EVLetin (EVReal [:: ("x", TReal)] 2%R) (EVPlus (EVVar [:: ("y", TReal); ("x", TReal)] "x") (EVVar [:: ("y", TReal); ("x", TReal)] "y")))).
+congr eval.
+do 2 congr Letin.
+congr Plus.
+congr Var; exact: Prop_irrelevance.
+congr Var; exact: Prop_irrelevance.
+Qed.
 
 End example.
 
 End lang_syntax_simple.
-
-Module withoutType.
-
-Section expression.
-Variables (R : realType).
-
-Inductive exp (g : list string) : Type :=
-| Real : R -> exp g
-| Var x : x \in g -> exp g
-(* | App t u : exp g (TArrow t u) -> exp g t -> exp g u *)
-| Letin x : exp g -> exp (x :: g) -> exp g
-| Plus : exp g -> exp g -> exp g
-.
-
-End expression.
-
-Arguments Real {R g}.
-Arguments Var {R g}.
-Arguments Letin {R g}.
-
-Declare Custom Entry exp.
-Notation "[ e ]" := e (e custom exp at level 5) : easylang_scope.
-Notation "x ':r'" := (@Real _ _ x%R) (in custom exp at level 1) 
-  : easylang_scope.
-Notation "x + y" := (Plus x y) (in custom exp at level 1) 
-  : easylang_scope.
-Notation "% x" := (Var x erefl) (in custom exp at level 1) 
-  : easylang_scope.
-Notation "'Let' x '<~' e 'In' f" := (Letin x e f)
-  (in custom exp at level 3,
-   x constr,
-   (* e custom expr at level 2, *)
-   f custom exp at level 2,
-   left associativity) : easylang_scope.
-Notation "{ x }" := x (in custom exp, x constr) : easylang_scope.
-Notation "x" := x (in custom exp at level 0, x ident) : easylang_scope.
-
-Local Open Scope easylang_scope.
-Section example.
-Variables (R : realType).
-
-Fail Definition e1 := [Let "x" <~ {1%:R}:r In %{"x"}].
-
-Fail Example e := [Let x <~ {1%:R}:r In
-                   Let y <~ {2%:R}:r In 
-                   %x] : exp R [::].
-
-Example e := [Let x <~ {1%:R}:r In
-               Let y <~ {2%:R}:r In 
-               {@Var R [:: y; x] x erefl}] : exp R [::].
-
-End example.
-End withoutType.
-
-
-Section eval.
-Variables (R : realType).
-
-End eval.
+End lang_syntax_simple.
